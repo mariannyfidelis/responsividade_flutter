56a8112f005e543f4b1ca5514fc88d4d15749924
Marianny Fidelis <mariannyfidelis@gmail.com>
2021-07-29 15:29:49 -0300

    Atualização do Futurebuilder para manipular uma lista de contas.

--- lib/app_gastos/screens/home_screen/components/body.dart
+++ lib/app_gastos/screens/home_screen/components/body.dart
@@ -1,3 +1,5 @@
+import 'package:app_responsividade_studio/app_gastos/models/conta.dart';
+import 'package:app_responsividade_studio/app_gastos/screens/home_screen/components/card_contas.dart';
 import 'package:app_responsividade_studio/app_gastos/services/conta_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,15 +12,17 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-
   ContaService _contaService = ContaService();
-  late Future<List> _loadContas;
+  late Future<List<Conta>> _loadContas;
+
+  //List<Conta>? _contas;
 
   @override
   void initState() {
     super.initState();
     _loadContas = _getContas();
   }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -28,20 +32,60 @@ class _BodyState extends State<Body> {
         children: [
           Container(
             color: Colors.deepPurple,
-            height: 175,
-            child: FutureBuilder(
+            height: 175.0,
+            child: FutureBuilder<List<Conta>>(
+              future: _loadContas,
               builder: (context, snapshot) {
-                return Card();
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasData) {
+                    List<Conta>? _contas;
+                    if (snapshot.data == null) {
+                      _contas = [];
+                    } else {
+                      _contas = snapshot.data;
+                    }
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: _contas!.length,
+                      padding: EdgeInsets.only(left: 16, right: 8),
+                      itemBuilder: (context, index) {
+                         return cardConta(
+                           context,
+                           _contas![index],
+                         );
+
+                      },
+                    );
+                  } else {
+                    return Center(
+                      child: Text(
+                        "Erro!",
+                        style: TextStyle(color: Color(0xFF07436A)),
+                      ),
+                    );
+                  }
+                } else {
+                  return CircularProgressIndicator(color: Colors.blueGrey);
+                }
               },
-              future: _contaService.gelAllContas(),
             ),
-          )
+          ),
+          Text("Restante da aplicação"),
+          carrousel(context),
         ],
       ),
     );
   }
 
-  Future<List> _getContas() async{
+  Future<List<Conta>> _getContas() async {
     return await _contaService.gelAllContas();
   }
+
+  // void atualizaContas(List<Conta>? data) {
+  //   print("@@@@${data!.length}");
+  //   for (int i = 0; i < data.length; i++) {
+  //     print(data[i].titulo);
+  //   }
+  // }
 }
